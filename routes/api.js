8c4b2e98002cf23c2e980f5aca5a3ac3fe87fac1
E-K8 <ekatplat@gmail.com>
2024-05-22 21:47:31 +0100

    add converting functionality for metric and imperial units

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
-  app.route('api/convert').get((req, res) => {
+  app.route('/api/convert').get((req, res) => {
     let input = req.query.input;
     let initNum = convertHandler.getNum(input);
     let initUnit = convertHandler.getUnit(input);
